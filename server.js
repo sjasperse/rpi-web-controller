52ce2cf60791021975e26784fb3bdbcef9aea695
Scott Jasperse <sjasperse@users.noreply.github.com>
2015-05-05 01:44:14 +0000

    Ready for the Pi

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
 var logger = require('morgan');
-
+var jfive = require('johnny-five');
+var Raspi = require('raspi-io');
+var board = new jfive.Board({
+  io: new Raspi() 
+});
 
 app.use(logger('dev'));
 app.use(express.static('app'));
 
+var relay = null;
 
 app.post('/action/on', function (req, res) {
 	console.log('ACTION: On');
+
+	relay.on();
 
 	res.sendStatus(200);
 });
@@ -16,22 +23,25 @@ app.post('/action/on', function (req, res) {
 app.post('/action/off', function (req, res) {
 	console.log('ACTION: Off');
 
+	relay.off();
+
 	res.sendStatus(200);
 });
 
 
 
 
+board.on('ready', function () {
+  relay = new jfive.Relay('P1-10');
 
+  var server = app.listen(3000, function() {
 
+    var host = server.address().address;
+    var port = server.address().port;
 
+    console.log('Example app listening at http://%s:%s', host, port);
 
-var server = app.listen(3000, function() {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  });
 
 });
 
